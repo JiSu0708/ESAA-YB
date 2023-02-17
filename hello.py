34429493408f0c1feea5d40f88eca95fcc110fee
JiSu0708 <125644597+JiSu0708@users.noreply.github.com>
2023-02-17 22:01:20 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
 # hello.py
 print("Hello World")
-"I ate %d apples. So I was sick for %s days." %(10, "three")
-"%10s" % "hi"
-"%-10s" %"hi"
-"%-10sjane" %"hi"
-"%0.4f" %3.42134234
-"%10.4f" % 3.123456543
+Hello World
