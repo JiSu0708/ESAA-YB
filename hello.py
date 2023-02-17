c888e00e4f596ad6dc07723c422c39b99f83550a
JiSu0708 <125644597+JiSu0708@users.noreply.github.com>
2023-02-17 21:57:18 +0900

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 # hello.py
 print("Hello World")
+"I ate %d apples. So I was sick for %s days." %(10, "three")
+"%10s" % "hi"
+"%-10s" %"hi"
+"%-10sjane" %"hi"
+"%0.4f" %3.42134234
+"%10.4f" % 3.123456543
